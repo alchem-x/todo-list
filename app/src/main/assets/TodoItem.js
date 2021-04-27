199de88e1035bb591ad5c8efd4af6ea5fe3d742d
cbdyzj <cbdyzj@jianzhao.org>
2021-04-27 21:21:00 +0800

    Update button style

--- app/src/main/assets/TodoItem.js
+++ app/src/main/assets/TodoItem.js
@@ -1,36 +1,36 @@
 export default {
-    template: `
-      <div class="border border-blue-500 p-2 my-2">
-      <div class="flex justify-between">
-        <span class="flex items-center">
-          <input @input="handleCheck" class="w-6 h-6 flex-none" type="checkbox" :checked="item.done">
-          <h3 class="ml-2 text-xl">{{ index }}. {{ item.title }}</h3>
-        </span>
-        <button @click="$emit('delete', index)" class="text-red-400 mt-1 text-xl flex-none px-0.5">
-          删除
-        </button>
-      </div>
-      <p class="text-base mt-2" v-if="item.content">
-        {{ item.content }}
-      </p>
-      </div>
-    `,
-    props: {
-        item: {
-            type: Object,
-            required: true,
-        },
-        index: {
-            type: Number,
-            required: true,
-        }
-    },
-    methods: {
-        handleCheck(ev) {
-            this.$emit('check', {
-                index: this.index,
-                checked: ev.target.checked,
-            })
-        },
-    }
+  template: `
+    <div class="border border-blue-500 p-2 my-2">
+    <div class="flex justify-between">
+      <span class="flex items-center">
+        <input @input="handleCheck" class="w-6 h-6 flex-none" type="checkbox" :checked="item.done">
+        <h3 class="ml-2 text-xl">{{ index }}. {{ item.title }}</h3>
+      </span>
+      <button @click="$emit('delete', index)" class="text-red-500 border border-red-500 text-xl flex-none px-1">
+        删除
+      </button>
+    </div>
+    <p class="text-base mt-2" v-if="item.content">
+      {{ item.content }}
+    </p>
+    </div>
+  `,
+  props: {
+      item: {
+          type: Object,
+          required: true,
+      },
+      index: {
+          type: Number,
+          required: true,
+      }
+  },
+  methods: {
+      handleCheck(ev) {
+          this.$emit('check', {
+              index: this.index,
+              checked: ev.target.checked,
+          })
+      },
+  }
 }
--- app/src/main/assets/Toolbar.js
+++ app/src/main/assets/Toolbar.js
@@ -1,53 +1,53 @@
 export default {
-    template: `
-      <div class="my-2">
-      <button class="text-blue-500 text-xl" v-if="!editing" @click="handleEdit">
-        新增
-      </button>
-      <form ref="form" class="mt-4" v-if="editing" @submit.prevent="handleSubmit">
-        <input name="title" placeholder="标题" type="text" @keydown.enter="handleSave"
-               @keydown.esc="handleCancel"
-               class="border-b outline-none w-full text-xl" required>
-        <textarea name="content" placeholder="内容" rows="5" @keydown.esc="handleCancel"
-                  class="border p-1 mt-2 outline-none w-full text-xl" />
-        <div class="mt-2 text-xl">
-          <button class="text-blue-500" @click="handleCancel">
-            取消
-          </button>
-          <button type="submit" class="text-blue-500">
-            保存
-          </button>
-        </div>
-      </form>
+  template: `
+    <div class="my-2">
+    <button class="text-blue-500 text-xl border border-blue-500 px-1" v-if="!editing" @click="handleEdit">
+      新增
+    </button>
+    <form ref="form" class="mt-4" v-if="editing" @submit.prevent="handleSubmit">
+      <input name="title" placeholder="标题" type="text" @keydown.enter="handleSave"
+             @keydown.esc="handleCancel"
+             class="border-b outline-none w-full text-xl" required>
+      <textarea name="content" placeholder="内容" rows="5" @keydown.esc="handleCancel"
+                class="border p-1 mt-2 outline-none w-full text-xl" />
+      <div class="mt-2 text-xl">
+        <button class="text-blue-500 border border-blue-500 px-1" @click="handleCancel">
+          取消
+        </button>
+        <button type="submit" class="text-blue-500 border border-blue-500 px-1">
+          保存
+        </button>
       </div>
-    `,
-    data() {
-        return {
-            editing: false
-        }
-    },
-    methods: {
-        handleEdit() {
-            this.editing = true
-            this.$nextTick(() => {
-                this.$refs.form.title.focus()
-            })
-        },
-        handleSubmit(ev) {
-            this.handleSave()
-        },
-        handleSave() {
-            const form = this.$refs.form
-            const title = String(form.title.value).trim()
-            const content = String(form.content.value).trim()
-            if (!title) {
-                return
-            }
-            this.$emit('add', { title, content })
-            this.editing = false
-        },
-        handleCancel() {
-            this.editing = false
-        },
-    }
+    </form>
+    </div>
+  `,
+  data() {
+      return {
+          editing: false
+      }
+  },
+  methods: {
+      handleEdit() {
+          this.editing = true
+          this.$nextTick(() => {
+              this.$refs.form.title.focus()
+          })
+      },
+      handleSubmit(ev) {
+          this.handleSave()
+      },
+      handleSave() {
+          const form = this.$refs.form
+          const title = String(form.title.value).trim()
+          const content = String(form.content.value).trim()
+          if (!title) {
+              return
+          }
+          this.$emit('add', { title, content })
+          this.editing = false
+      },
+      handleCancel() {
+          this.editing = false
+      },
+  }
 }
