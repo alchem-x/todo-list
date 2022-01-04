649ea731de683535e43a399e2588cb4219b44a45
cbdyzj <cbdyzj@jianzhao.org>
2022-01-04 22:25:29 +0800

    Use Kotlin as Gradle Build Scripts

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    compileSdk = 30
+    buildToolsVersion = "30.0.2"
+
+    defaultConfig {
+        applicationId = "alchemy.works.todo"
+        minSdk = 28
+        targetSdk = 30
+        versionCode = 2
+        versionName = "1.0.1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
